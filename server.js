ab9d29ccf8b14b8d8a43f2a5fa1a81f04f66aefc
David <HL_David@outlook.com>
2021-12-24 02:17:17 -0600

    link created

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+//module required
+const mongoose = require('mongoose');
+const dns = require('dns');
+
 const app = express();
 
 // Basic Configuration
@@ -9,6 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({extended:false}));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -25,3 +30,67 @@ app.listen(port, function() {
 
 
 //Code created by David Hernandez (RaveFuzzball)
+//Mongoose connection
+mongoose.connect(process.env.URI)
+  .then(() => console.log('DB is Connected'))
+  .catch(err => console.error(err));
+//Mongoose link model
+const linkModel = mongoose.Schema({
+  original: String,
+  short: Number
+}); 
+
+const link = mongoose.model('link',linkModel);
+//Functions
+function randomNumber(max){
+  return Math.floor(Math.random() * max) + 1;
+}
+
+function cutLink(fullLink){
+  if(fullLink.includes('http')){
+    iCut = fullLink.indexOf('/') + 2;
+    return fullLink.substring(iCut);
+  }else{
+    return fullLink
+  }
+}
+
+async function linkCreate(req, res) {
+  let shortUrl = randomNumber(10000);
+  const linkSearched = await link.findOne({short: shortUrl});
+  if(linkSearched){
+    linkCreate();
+  }else{
+    const linkSearchedByName = await link.findOne({original: req.body.url});
+    if(linkSearchedByName){
+      res.json({ original_url : linkSearchedByName.original, short_url : linkSearchedByName.short});
+    }else{
+      var newLink = new link({
+        original: req.body.url,
+        short: shortUrl
+      });
+      await newLink.save();
+      res.json({ original_url : newLink.original, short_url : newLink.short});
+    }
+  }
+}
+
+//Create a new short url
+app.post('/api/shorturl', (req, res) => {
+  dns.lookup(cutLink(req.body.url),(err) => {
+    if(err){
+      res.json({error: 'Invalid url'});
+      return
+    }else{
+      linkCreate(req, res);
+      return
+    }
+  });
+  
+});
+
+//Redirect to short url
+app.get('/api/shorturl/:short_url',async (req, res) => {
+  const url = await link.findOne({short:req.params.short_url}); 
+  res.redirect(url.original);
+});
